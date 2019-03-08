112bc20c2b535c255cf1386188030865d5c9f3b2
ferianandhika <48312253+ferianandhika@users.noreply.github.com>
2019-03-08 08:32:40 +0700

    Add files via upload

---
+++ FerianAndhikaT.java
@@ -0,0 +1,16 @@
+
+package ferianandhikat;
+
+
+public class FerianAndhikaT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama  : Ferian Andhika Toasi\n");
+        System.out.println("NIM   : 17090095\n");
+        System.out.println("Kelas : TI 4A");
+    }
+    
+}
